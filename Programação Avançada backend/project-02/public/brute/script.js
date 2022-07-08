90de87ab02067ba7ce5df4edd43747b0b91b96b9
Bruno-Patrick <021996.bmx@gmail.com>
2022-07-07 19:53:39 -0500

    Alterações na estilização e HTML

--- Programação Avançada backend/project-02/public/brute/script.js
+++ Programação Avançada backend/project-02/public/brute/script.js
@@ -1,6 +1,6 @@
-const ul = document.querySelector('ul')
 const input = document.querySelector('input')
 const form = document.querySelector('form')
+const cardsgroup = document.getElementById('cards-group')
 
 async function load(){
     const res = await fetch('http://localhost:3000/')
@@ -10,38 +10,34 @@ async function load(){
 
 function addElement({ name, url }) {
     //...
-    fetch(`http://localhost:3000/?name=${name}&url=${url}`)
-    const tbody = document.getElementById('content-table') //Captura o 
-    const tr = document.createElement('tr') //Cria um elemento <li></li>
-    const thName = document.createElement('th')
-    const thURL = document.createElement('th')
-    const thAction = document.createElement('th')
-    const deleteButton = document.createElement('button') //Cria um elemento <i></i>
-    const a = document.createElement('a')
+    const divExt = document.createElement('div')
+    divExt.classList.add('card')
+    divExt.classList.add('text-center')
 
-    a.setAttribute('href',`${url}`)
-    a.setAttribute("target","_blank")
-    a.textContent = `${url}`
+    const divInt = document.createElement('div')
+    divInt.classList.add('card-head')
 
-    deleteButton.classList.add('btn') 
-    deleteButton.classList.add('btn-sm') 
-    deleteButton.classList.add('btn-warning') 
-    deleteButton.classList.add('font-weight-bold') 
-    deleteButton.textContent = 'Apagar'
+    const trash = document.createElement('img')
+    trash.setAttribute('src','https://i.imgur.com/fIXZoHb.png')
+    trash.classList.add('trash')
 
-    thName.ATTRIBUTE_NODE = 'scope=row'
-    thName.textContent = `${name}`
+    const edit = document.createElement('img')
+    edit.setAttribute('src','https://i.imgur.com/HZaPoQb.png')
+    edit.classList.add('edit')
 
-    thURL.ATTRIBUTE_NODE = 'scope=row'
-    thURL.appendChild(a)
+    const link = document.createElement('a')
+    link.setAttribute('href',`${url}`)
+    link.setAttribute('target','_blank')
+    link.textContent = `${name}`
 
-    thAction.appendChild(deleteButton)
-    tr.appendChild(thName)
-    tr.appendChild(thURL)
-    tr.appendChild(thAction)
-    tbody.appendChild(tr)
+    divInt.appendChild(trash)
+    divInt.appendChild(edit)
+    divExt.appendChild(divInt)
+    divExt.appendChild(link)
+    cardsgroup.appendChild(divExt)
 
-    removeElement(deleteButton, {name, url}) // Callback para função
+
+    removeElement(trash, {name, url}) // Callback para função
 }
 
 function removeElement(element, {name, url}) {
@@ -49,15 +45,8 @@ function removeElement(element, {name, url}) {
     element.addEventListener('click', () => { //Adiciona um event listener no ícone
         if (confirm('Tem certeza que quer deletar?')){
             fetch(`http://localhost:3000/?name=${name}&url=${url}&del=1`)
-            element.parentNode.parentNode.remove() //remove o elemento anterior, no caso toda a estrutura da <li>
+            element.parentNode.parentNode.remove() //remove o elemento anterior, no caso toda a estrutura da <card>
         }
-    })
-
-    element.addEventListener('mousemove', () => {
-        element.parentNode.parentNode.classList.add('delete')
-    })
-    element.addEventListener('mouseleave', () => {
-        element.parentNode.parentNode.classList.remove('delete')
     })
 }
 
@@ -78,6 +67,7 @@ form.addEventListener('submit', (event) => {
         return alert('Digite a url da maneira correta.')
 
     addElement({ name, url })
+    fetch(`http://localhost:3000/?name=${name}&url=${url}`)
 
     input.value = ''
 })
